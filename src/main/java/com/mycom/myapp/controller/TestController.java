df918f814ff8bb4f7429d344315a8598d1ce9882
0andrich <rlrpdj@gmail.com>
2025-10-23 13:14:12 +0900

    first

---
+++ src/main/java/com/mycom/myapp/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.mycom.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		System.out.println("hello");
+		return "/hello.html";
+	}
+}
