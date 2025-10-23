88a8b4050865b6d54bd58f06370b5f262adf196f
0andrich <rlrpdj@gmail.com>
2025-10-23 16:38:00 +0900

    update 2

--- src/main/java/com/mycom/myapp/controller/TestController2.java
+++ src/main/java/com/mycom/myapp/controller/TestController2.java
@@ -3,8 +3,8 @@ package com.mycom.myapp.controller;
 public class TestController2 {
 	public void m() {
 		System.out.println("TestController2-m()");
-		System.out.println("TestController2-before()");
 		System.out.println("TestController2-m()");
+		System.out.println("TestController2-before()");
 		System.out.println("TestController2-after()");
 	}
 }
